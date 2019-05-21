7a7376d16445d2883a5ae2640d3da4e194ece7f9
Ofir Cohen <37571916+Play38@users.noreply.github.com>
2019-05-21 16:51:45 +0300

    Google maps has been implamented

--- src/Screens/GeoLocation.js
+++ src/Screens/GeoLocation.js
@@ -1,36 +1,93 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, Alert } from 'react-native'
+import { Text, View, TouchableOpacity, Alert, StyleSheet } from 'react-native'
 import styles from './containerStyle'
+import MapView from 'react-native-maps'
+
+const stylesMap = StyleSheet.create({
+  mapContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: '90%'
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  mapText: {
+    position: 'absolute',
+    top: 20,
+    alignItems: 'center'
+  }
+})
+
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       location: null,
-      location2: null
+      location2: null,
+      click: 0
     }
   }
 
-  findCoordinates = () => {
+    handlePressFindCoordinates = () => {
     navigator.geolocation.getCurrentPosition(
       position => {
-        const location = JSON.stringify(position.coords.longitude)
-        const location2 = JSON.stringify(position.coords.latitude)
+        const click = 1
+        const location = position.coords.longitude
+        const location2 = position.coords.latitude
 
-        this.setState({ location, location2 })
+        this.setState({ location, location2, click })
       },
       error => Alert.alert(error.message),
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
     )
   }
-
+  mapClick() {
+    const stack = []
+    if (this.state.click === 1) {
+      stack.push(
+        <View style={stylesMap.mapContainer}>
+          <MapView
+            style={stylesMap.map}
+            showsUserLocation
+            region={{
+              latitude: this.state.location2,
+              latitudeDelta: 0.1,
+              longitude: this.state.location,
+              longitudeDelta: 0.1
+            }}
+          >
+            <MapView.Marker
+              coordinate={{ latitude: this.state.location2, longitude: this.state.location }}
+            />
+          </MapView>
+        </View>
+      )
+    }
+    return stack
+  }
+  TextClick() {
+    const stack = []
+    stack.push(
+      <TouchableOpacity style={stylesMap.mapText} onPress={this.handlePressFindCoordinates}>
+        <Text>Click on me to get your current location</Text>
+      </TouchableOpacity>
+    )
+    return stack
+  }
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={this.findCoordinates}>
-          <Text style={styles.welcome}>Click on me to get GeoLocation</Text>
-          <Text>longitude: {this.state.location}</Text>
-          <Text>latitude: {this.state.location2}</Text>
-        </TouchableOpacity>
+        {this.TextClick()}
+        {this.mapClick()}
       </View>
     )
   }
